800266d3879adf05fd3c1c9c09cbac5c84f88382
Eric Friedman <ericsean01@gmail.com>
2020-02-25 22:16:38 -0500

    jquery updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -435,7 +435,7 @@ jQuery(document).ready(function() {
     dottedOverlay: "none",
     delay: 16000,
     startwidth: 1170,
-    startheight: 645,
+    startheight: 640,
     hideThumbs: 200,
     thumbWidth: 100,
     thumbHeight: 50,
